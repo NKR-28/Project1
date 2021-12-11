a48508d5b9b1318112ee808e185610557f57ea6f
Avalynn Judge <avalynnjudge@gmail.com>
2021-12-11 13:20:59 -0800

    Added API calls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
 // Comment!
+
+
+var random_api_url = 'https://randomuser.me/api/';
+
+$.ajax({
+    url: random_api_url,
+    dataType: 'json',
+    success: function(data) {
+      console.log(data.results[0].name.first, data.results[0].name.last);
+    }
+});
+
+
+var dnd_api_url = 'https://www.dnd5eapi.co/api/';
+
+$.ajax({
+    url: dnd_api_url,
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    }
+});
+
