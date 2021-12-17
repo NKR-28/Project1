5894b17e902d0d640f309101e15ba866fb10aabd
Avalynn Judge <avalynnjudge@gmail.com>
2021-12-16 19:33:39 -0800

    Added Additional Functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,17 @@
 var random_api_url = 'https://randomuser.me/api/';
 var dnd_api_url = 'https://www.dnd5eapi.co/api/';
 var dnd_api_skills = 'https://www.dnd5eapi.co';
-var modifiers_array = [];
+
+
 var ability_scores_array = [];
 var skills_array = [];
 var armor_class;
+var maximum_HP;
+var initiative;
 
 
 
 
-
-
-function generate_random_class() {
-  $.ajax({
-    url: dnd_api_url + "classes",
-    dataType: 'json',
-    success: function(data) {
-
-      var randomClass = data.results[Math.floor(Math.random()*data.results.length)];
-
-      console.log(randomClass.name);
-    }
-  });
-};
 
 
 
@@ -35,7 +24,7 @@ function generate_random_name() {
     url: random_api_url,
     dataType: 'json',
     success: function(data) {
-      console.log(data.results[0].name.first, data.results[0].name.last);
+      console.log("Name: "+ data.results[0].name.first, data.results[0].name.last);
     }
   });
 };
@@ -49,7 +38,7 @@ function generate_random_alignment() {
     dataType: 'json',
     success: function(data) {
       var randomAlignment = data.results[Math.floor(Math.random()*data.results.length)];
-      console.log(randomAlignment.name);
+      console.log("Alignment: " + randomAlignment.name);
     }
   });
 };
@@ -62,15 +51,38 @@ function generate_random_race() {
     dataType: 'json',
     success: function(data) {
       var randomRace = data.results[Math.floor(Math.random()*data.results.length)];
-      console.log(randomRace.name);
+      console.log("Race: " + randomRace.name);
+
+      generate_random_class(randomRace.name);
     }
   });
 };
 
+
+
+
+function generate_random_class(randomRace) {
+  $.ajax({
+    url: dnd_api_url + "classes",
+    dataType: 'json',
+    success: function(data) {
+      var randomClass = data.results[Math.floor(Math.random()*data.results.length)];
+      console.log("Class: " + randomClass.name);
+
+      generate_ability_scores(randomRace,randomClass);
+    }
+  });
+};
+
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+
 //Random Ability Scores
-
-
-function generate_ability_scores() {
+function generate_ability_scores(randomRace, randomClass) {
   // Randomly assign ability scores
   $.ajax({
     url: dnd_api_url + "ability-scores",
@@ -84,13 +96,153 @@ function generate_ability_scores() {
         var scoreIndex = standard_array.indexOf(random_score);
         standard_array.splice(scoreIndex, 1);
         ability_scores_array[index] = [data.results[index].name, random_score, Math.floor(((random_score)-10)/2)];
-        modifiers_array[index] = ability_scores_array[index][2];
       });
+
+
+      if (randomRace == "Dragonborn") {
+        // STR+2
+        ability_scores_array[4][1] = (ability_scores_array[4][1] + 2);
+        ability_scores_array[4][2] = Math.floor(((ability_scores_array[4][1])-10)/2);
+        // CHA+1
+        ability_scores_array[0][1] = (ability_scores_array[0][1] + 1);
+        ability_scores_array[0][2] = Math.floor(((ability_scores_array[0][1])-10)/2);
+      }
+      else if (randomRace == "Dwarf") {
+        // CON+2
+        ability_scores_array[1][1] = (ability_scores_array[1][1] + 2);
+        ability_scores_array[1][2] = Math.floor(((ability_scores_array[1][1])-10)/2);
+      }
+      else if (randomRace == "Elf") {
+        // DEX+2
+        ability_scores_array[2][1] = (ability_scores_array[2][1] + 2);
+        ability_scores_array[2][2] = Math.floor(((ability_scores_array[2][1])-10)/2);
+      }
+      else if (randomRace == "Gnome") {
+        // INT+2
+        ability_scores_array[3][1] = (ability_scores_array[3][1] + 2);
+        ability_scores_array[3][2] = Math.floor(((ability_scores_array[3][1])-10)/2);
+      }
+      else if (randomRace == "Half-Elf") {
+        // INT+2
+        ability_scores_array[3][1] = (ability_scores_array[3][1] + 2);
+        ability_scores_array[3][2] = Math.floor(((ability_scores_array[3][1])-10)/2);
+
+        // Random Ability Score +1 X2
+        let random_skill = getRandomInt(6);
+        ability_scores_array[random_skill][1] = (ability_scores_array[random_skill][1] + 1);
+        ability_scores_array[random_skill][2] = Math.floor(((ability_scores_array[random_skill][1])-10)/2);
+
+        random_skill = getRandomInt(6);
+        ability_scores_array[random_skill][1] = (ability_scores_array[random_skill][1] + 1);
+        ability_scores_array[random_skill][2] = Math.floor(((ability_scores_array[random_skill][1])-10)/2);
+      }
+      else if (randomRace == "Half-Orc") {
+        // STR+2
+        ability_scores_array[4][1] = (ability_scores_array[4][1] + 2);
+        ability_scores_array[4][2] = Math.floor(((ability_scores_array[4][1])-10)/2);
+
+        // CON+1
+        ability_scores_array[1][1] = (ability_scores_array[1][1] + 1);
+        ability_scores_array[1][2] = Math.floor(((ability_scores_array[1][1])-10)/2);
+      }
+      else if (randomRace == "Halfling") {
+        // DEX+2
+        ability_scores_array[2][1] = (ability_scores_array[2][1] + 2);
+        ability_scores_array[2][2] = Math.floor(((ability_scores_array[2][1])-10)/2);
+      }
+      else if (randomRace == "Human") {
+        // +1 All Ability Scores
+        ability_scores_array[0][1] = (ability_scores_array[0][1] + 1);
+        ability_scores_array[0][2] = Math.floor(((ability_scores_array[0][1])-10)/2);
+        ability_scores_array[1][1] = (ability_scores_array[1][1] + 1);
+        ability_scores_array[1][2] = Math.floor(((ability_scores_array[1][1])-10)/2);
+        ability_scores_array[2][1] = (ability_scores_array[2][1] + 1);
+        ability_scores_array[2][2] = Math.floor(((ability_scores_array[2][1])-10)/2);
+        ability_scores_array[3][1] = (ability_scores_array[3][1] + 1);
+        ability_scores_array[3][2] = Math.floor(((ability_scores_array[3][1])-10)/2);
+        ability_scores_array[4][1] = (ability_scores_array[4][1] + 1);
+        ability_scores_array[4][2] = Math.floor(((ability_scores_array[4][1])-10)/2);
+        ability_scores_array[5][1] = (ability_scores_array[5][1] + 1);
+        ability_scores_array[5][2] = Math.floor(((ability_scores_array[5][1])-10)/2);
+      }
+      else if (randomRace == "Halfling") {
+        // DEX+2
+        ability_scores_array[2][1] = (ability_scores_array[2][1] + 2);
+        ability_scores_array[2][2] = Math.floor(((ability_scores_array[2][1])-10)/2);
+      }
+      else if (randomRace == "Tiefling") {
+        // CHA+2
+        ability_scores_array[0][1] = (ability_scores_array[0][1] + 2);
+        ability_scores_array[0][2] = Math.floor(((ability_scores_array[0][1])-10)/2);
+
+        // INT+1
+        ability_scores_array[3][1] = (ability_scores_array[3][1] + 1);
+        ability_scores_array[3][2] = Math.floor(((ability_scores_array[3][1])-10)/2);
+      }
+
+
+
+
+
+      if (randomClass.name == "Barbarian") { 
+        maximum_HP = (12 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Bard") { 
+        maximum_HP = (8 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Cleric") { 
+        maximum_HP = (8 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Druid") { 
+        maximum_HP = (8 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Fighter") { 
+        maximum_HP = (10 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Monk") { 
+        maximum_HP = (8 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Paladin") { 
+        maximum_HP = (10 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Ranger") { 
+        maximum_HP = (10 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Rogue") { 
+        maximum_HP = (8 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Sorcerer") { 
+        maximum_HP = (6 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Warlock") { 
+        maximum_HP = (8 + ability_scores_array[1][2]);
+      }
+      else if (randomClass.name == "Wizard") { 
+        maximum_HP = (6 + ability_scores_array[1][2]);
+      }
+      else {
+        maximum_HP = (10 + ability_scores_array[1][2]);
+      }
+
+      initiative = ability_scores_array[2][2];
+      if (initiative < 0) {
+        console.log("Initiative Modifier: " + initiative);
+      }
+      else {
+        console.log("Initiative Modifier: +" + initiative);
+      }
+
+
+      //maximum_HP = ability_scores_array[1][2];
+      console.log("Max HP: " + maximum_HP);
+
+      armor_class = 10+ability_scores_array[2][2];
+      console.log("Armor Class: " + armor_class);
     }
   });
 
 
-  //Random Skills
+  //Creates Skill array from ability score array
   $.ajax({
     url: dnd_api_url + "skills",
     dataType: 'json',
@@ -106,29 +258,22 @@ function generate_ability_scores() {
                 skills_array[index] = [data.name, data.ability_score.name, ability_scores_array[i][2]]
               }
             };
-            armor_class = (10+ability_scores_array[2][2]);
-            //console.log(armor_class);
           }
         });
       });
-      //console.log(skills_array);
     }
   });
-  console.log(ability_scores_array);
-  console.log(armor_class);
-
-  //var armor_class = (10+(ability_scores_array[2][2]));
-
 };
 
 
 function generate_character() {
   generate_random_name();
-  generate_ability_scores();
   generate_random_race();
   generate_random_alignment();
-  generate_random_class();
 
+  console.log(skills_array);
+  //console.log("Armor Class: " + armor_class);
+  console.log(ability_scores_array);
 };
 
 generate_character();
